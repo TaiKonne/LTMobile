c264471da788910266d4f2a3ab771e130656bed0
TaiKonne <2124802010067@student.tdmu.edu.vn>
2024-09-22 00:39:19 +0700

    Firstpush

---
+++ app/src/main/java/com/example/myapplication/activity/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.myapplication.activity
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import com.example.myapplication.Model.SliderModel
+import com.example.myapplication.R
+import com.example.myapplication.SliderAdapter
+import com.example.myapplication.ViewModel.MainViewModel
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    private val viewModel = MainViewModel()
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBanner()
+    }
+
+    private fun initBanner() {
+        binding.progressBarBanner.visibility = View.VISIBLE
+        viewModel.banners.observe(this, Observer { items ->
+            banners(items)
+            binding.progressBarBanner.visibility=View.GONE
+        })
+        viewModel.loadBanners()
+    }
+
+    private fun banners(images: List<SliderModel>) {
+        binding.viewpagerSlider.adapter = SliderAdapter(images, binding.viewpagerSlider)
+        binding.viewpagerSlider.clipToPadding = false
+        binding.viewpagerSlider.clipChildren = false
+        binding.viewpagerSlider.offscreenPageLimit = 3
+        binding.viewpagerSlider.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+
+        val compositePageTransformer = CompositePageTransformer().apply {
+            addTransformer(MarginPageTransformer(40))
+        }
+        binding.viewpagerSlider.setPageTransformer(compositePageTransformer)
+        if (images.size > 1) {
+            binding.dotIndicator.visibility = View.VISIBLE
+            binding.dotIndicator.attachTo(binding.viewpagerSlider)
+        }
+    }
+}
+
